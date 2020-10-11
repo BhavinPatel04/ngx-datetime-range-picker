107fa0ab839eef94694501c354b1c84b3a2a18ab
bhavinpatel@ebay.com <bhavinpatel@ebay.com>
2020-10-10 19:45:12 -0700

    upgraded to angular 8

--- projects/ngx-datetime-range-picker/src/lib/material/material.module.ts
+++ projects/ngx-datetime-range-picker/src/lib/material/material.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from "@angular/core";
-import { MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSelectModule } from "@angular/material";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
 
 const modules = [MatFormFieldModule, MatButtonModule, MatInputModule, MatIconModule, MatSelectModule];
 @NgModule({
---
+++ src/config/config.ts
@@ -0,0 +1,8 @@
+/**
+ * The file contents for the current environment
+ * will overwrite these during build.
+ */
+
+export const Config = {
+  production: false
+};
